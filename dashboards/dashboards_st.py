cf6222f6235cee0ce510d54304b41b75d8dd3e50
Bai Chen <35583877+baichen111@users.noreply.github.com>
2024-09-24 21:38:24 +0800

    Add files via upload

--- dashboards/dashboards_st.py
+++ dashboards/dashboards_st.py
@@ -15,6 +15,7 @@ options = st.sidebar.radio('Select what you want to display:', ['Watch List', 'P
 
 cols_1 = st.columns(2)
 cols_2 = st.columns(2)
+cols_3 = st.columns(2)
 
 if options == 'Watch List':
     st.title("Stock Watchlist")
@@ -72,13 +73,30 @@ elif options == 'Plot':
     #plot return for each asset
     return_df = portfolio_df[['total_return']][:-2]
     return_df['positive_negative'] = return_df["total_return"].apply(lambda x:float(x[:-1])) > 0
-    bar_assets = px.bar(return_df,x=return_df.index,y='total_return',text_auto=True,
+    bar_assets_return = px.bar(return_df,x=return_df.index,y='total_return',text_auto=True,
                         color='positive_negative',
                         color_discrete_map={True: "green", False: "red"},
-                        title='Assets Total Return %')
-    st.plotly_chart(bar_assets)
+                        title='Assets Total Return %',
+                        labels={'total_return':'Total return %'})
+    bar_assets_return.layout.showlegend = False
+    st.plotly_chart(bar_assets_return)
     
-    
-    
-    
-    
+    #plot assets value 
+    value_df = portfolio_df[['unrealizedPNL']][:-2]
+    bar_assets_value = px.bar(value_df,x=value_df.index,y='unrealizedPNL',text_auto=True,
+                        color='unrealizedPNL',
+                        # color_discrete_map={True: "green", False: "red"},
+                        color_continuous_scale=px.colors.sequential.Viridis,
+                        # color_continuous_scale='Inferno',
+                        # color_continuous_scale='Bluered_r',
+                        # color_continuous_scale=px.colors.sequential.Cividis_r,
+                        # color_continuous_scale=["red", "green", "blue"],
+                        # color_continuous_scale=px.colors.diverging.BrBG,
+                        labels={
+                     "unrealizedPNL": "Asset Profit $ ",
+
+                 },
+                        
+                        title='Assets Profit in USD')
+    bar_assets_value.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False, texttemplate='%{y:.2f}')
+    st.plotly_chart(bar_assets_value)
