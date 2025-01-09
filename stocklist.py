3b55b205c94678fc257026aa697d1fd66546f08d
Bai Chen <35583877+baichen111@users.noreply.github.com>
2025-01-09 16:38:21 +0800

    Add files via upload

---
+++ stocklist.py
@@ -0,0 +1,4 @@
+syms = ['NVDA','AMD','MRVL','MSFT','GOOG','AAPL','PLTR','MU','AVGO','TSM','META',
+        'LMT','AMZN','ADBE','SMCI','ORCL','TSLA','ASML','SHOP','U','CRWD','SNOW','DDOG','NOW','HIMS']
+fields = ['sector','beta','forwardPE','profitMargins','currentRatio','returnOnEquity',
+                       'grossMargins','operatingMargins','ebitdaMargins','earningsGrowth','revenueGrowth','debtToEquity']
